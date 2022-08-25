298046bbde6c3ccd43ce171f9cded9ccf9a39103
Cc <Cc@163.com>
2022-08-25 10:49:55 +0800

    hot-fix commit first

--- src/main/java/git.java
+++ src/main/java/git.java
@@ -1,5 +1,8 @@
 public class git {
     public static void main(String[] args) {
         System.out.println("test1");
+        System.out.println("test2");
+        System.out.println("test3");
+        System.out.println("test4");
     }
 }
